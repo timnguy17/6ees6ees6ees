077beb2495dbb8161c4243797986899f2c7767a8
Timothy <timnguy17@gmail.com>
2022-03-07 10:35:09 -0800

    pass all tests for 6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
-};
+  constructor () {
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+
+  }
+
+  gamble (treasure) {
+    this.treasureChest.push(treasure);
+
+  }
+  forage () {
+    return 'I am too old, let me play cards instead';
+  }
+}
